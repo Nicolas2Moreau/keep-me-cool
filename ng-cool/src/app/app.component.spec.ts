e0c3abeb18ab3bf68dd1085668b5956a9ca9b091
nicolas2moreau <nicolas2moreau@gmail.com>
2023-10-31 08:56:54 +0100

    setup(app)project reboot +working forge + WIP external links

--- ng-cool/src/app/app.component.spec.ts
+++ ng-cool/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'keepcool-app'`, () => {
+  it(`should have as title 'ng-cool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('keepcool-app');
+    expect(app.title).toEqual('ng-cool');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('keepcool-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-cool app is running!');
   });
 });
--- ng-cool/src/app/app.component.ts
+++ ng-cool/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'keepcool-app';
+  title = 'Keep Me Cool';
   actualVersion = '1.0.0';
   availableVersion = '1.0.0';
 }
